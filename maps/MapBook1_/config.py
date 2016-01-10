c5116c9a53024354c1261209710e6fb854327e98
Gryffyn <gryffyn@archlinux.net>
2016-01-10 17:13:27 +0000

    fixed map

--- maps/MapBook1_/config.py
+++ maps/MapBook1_/config.py
@@ -1,3 +1,1 @@
-﻿config = {'spawn_pos': [30.0, 40.0], 'level_dimensions': [50, 50], 'music': 'Fantasy.ogg'}
-=======
-config = {'spawn_pos': [5.0, 5.0], 'level_dimensions': [50, 50], 'music': 'Fantasy.ogg'}
+config = {'spawn_pos': [30.0, 40.0], 'level_dimensions': [50, 50], 'music': 'Fantasy.ogg'}
--- maps/MapBook1_/triggers.py
+++ maps/MapBook1_/triggers.py
@@ -1,3 +1,1 @@
-﻿triggers = [{'width': 1.0, 'x': 42.984375, 'deactivate_after_use': True, 'trigger': ['CHANGEMAP', 'MapBook2'], 'y': 42.984375, 'height': 1.0}]
-=======
-triggers = [{'width': 1.0, 'y': 22.9609375, 'height': 1.0, 'x': 18.9765625, 'trigger': ['TRYWINGAME']}, {'width': 1.0, 'trigger': ['CHANGEMAP', 'TheMap1'], 'y': 42.984375, 'height': 1.0, 'x': 42.984375, 'deactivate_after_use': True}]
+triggers = [{'width': 1.0, 'x': 42.984375, 'deactivate_after_use': True, 'trigger': ['CHANGEMAP', 'MapBook2'], 'y': 42.984375, 'height': 1.0}]
