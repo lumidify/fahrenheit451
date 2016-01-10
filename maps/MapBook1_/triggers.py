cc0a14a51f0cbc94ddca046419b0501831e5c79e
Gryffyn <gryffyn@archlinux.net>
2016-01-10 18:48:53 +0000

    updated

--- maps/MapBook1_/triggers.py
+++ maps/MapBook1_/triggers.py
@@ -1,1 +1,1 @@
-triggers = [{'width': 1.0, 'x': 42.984375, 'deactivate_after_use': True, 'trigger': ['CHANGEMAP', 'MapBook2'], 'y': 42.984375, 'height': 1.0}]
+triggers = [{'height': 1.0, 'width': 1.0, 'deactivate_after_use': True, 'trigger': ['CHANGEMAP', 'maps/MapBook2'], 'x': 42.9375, 'y': 42.9375}]
